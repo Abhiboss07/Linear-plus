d15ab58426d23cacc754789fae0b0dddfa21db40
Abhiboss07 <abhicps19@gmail.com>
2026-01-13 20:20:14 +0530

    polish UI and demo prep

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"+W8/RBNHcDvBCYVrodDHn/x1kgGkKMyVjoKnL3b39x8=\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"lNMN15rboW50MtZQT2EuoZU+tHA7SHrSRXpPLKhqQQM=\"\n}"
